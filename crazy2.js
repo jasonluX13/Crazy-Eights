9cb9625aba58e237a07a9e2bb24b196c02cd7497
Jason Lu <jasonluX13@gmail.com>
2019-09-26 12:04:14 -0400

    Added get hands

--- crazy2.js
+++ crazy2.js
@@ -8,6 +8,17 @@
     async function getDeck(deckId){
         return await fetch(baseUrl + deckId + '/');
     }
+
+    async function drawCards(deckId, n){
+        return await fetch(baseUrl + deckId + '/draw/?count=' + n);
+    }
+    async function putCardInPile(deckId, pileName, cards){
+        let cardCodes = cards.map(card => card.code).join(',');
+        return await fetch(baseUrl + deckId + '/pile/' + pileName + '/add/?cards=' + cardCodes);
+    }
+    async function getCardsInPile(deckId, pileName){
+        return await fetch(baseUrl + deckId + '/pile/' + pileName + '/list/');
+    }
     let deckId = localStorage.getItem(deckIdKey);
    
     let response = undefined;
@@ -22,8 +33,44 @@
   
     deckId = deckInfo.deck_id;
     localStorage.setItem(deckIdKey, deckId);
-    
-
     console.log(deckId, remaining);
+
+    let computerHand = [];
+    let humanHand = [];
+    let discardPile = [];
+
+    //Check if game has started
+    if (remaining === 52){ 
+        const response = await drawCards(deckId, 11);
+        const cardsInfo = await response.json();
+        const cards = cardsInfo.cards;
+        computerHand = cards.slice(0, 5);
+        humanHand = cards.slice(5, 10);
+        discardPile = cards.slice(10);
+        await Promise.all([
+            putCardInPile(deckId, 'human', humanHand),
+            putCardInPile(deckId, 'computer', computerHand),
+            putCardInPile(deckId, 'discard', discardPile),
+        ]);
+        
+    } else {
+        const responses = await Promise.all([
+            getCardsInPile(deckId, 'human'),
+            getCardsInPile(deckId, 'computer'),
+            getCardsInPile(deckId, 'discard'),
+        ]);
+        const pileInfos = await Promise.all(responses.map(response => response.json()));
+        humanHand = pileInfos[0].piles.human.cards;
+        computerHand = pileInfos[1].piles.computer.cards;
+        discardPile = pileInfos[2].piles.discard.cards;
+    }
+    const humanImages = humanHand
+        .map(card => card.image)
+        .map(url => '<img src="'+ url + '" class="upcard">');
+    document.getElementById('human').innerHTML = humanImages.join('');
+    
+    console.log('computer: ' , computerHand);
+    console.log('human: ', humanHand);
+    console.log('discard: ', discardPile);
 
 })();
